5b2b53ad2b418039a5c57e9572875091e2ef8310
mm <meirav.malka@gmail.com>
2026-01-24 22:02:58 +0200

    added lots of stuff

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import requests
-def main():
-    #make a request
-    consumer_key = 'VQfSVciGUlwU230o4q9SgMAyyrg6EBoy'
-    response = requests.get(f"https://app.ticketmaster.com/discovery/v2/events.json?apikey={consumer_key}")
-    print(response.text)
-if __name__ == '__main__':
-    main()
+import streamlit as st
+
+from src.ticket_request import TicketRequest
+from src.data_analysis import DataAnalysis
+url = "https://app.ticketmaster.com/discovery/v2/events.json"
+
+tr = TicketRequest(url).call_api()
+
+
+st.title('Your Ticket Master App')
+
+name = st.text_input('Enter your name', '')
+if name:
+    st.write(f'Hello {name}, welcome to the custom ticket master app!')
+    da = DataAnalysis(tr).startup()
---
+++ src/ticket_request.py
@@ -0,0 +1,30 @@
+import requests
+import toml
+
+from pathlib import Path
+
+class TicketRequest:
+
+    def __init__(self, url):
+        self.api_key = self.get_secret()
+        self.url = url
+
+
+    def call_api(self):
+        response = requests.get(self.url, params={"apikey": self.api_key})
+        return response.json()
+
+
+    @staticmethod
+    def get_secret():
+        try:
+            project_root = Path(__file__).resolve().parents[1]
+            secrets_path = project_root / ".streamlit" / "secrets.toml"
+            secrets = toml.load(secrets_path)
+            return secrets["TICKETMASTER_API_KEY"]
+
+        except FileNotFoundError:
+            raise RuntimeError("secrets.toml not found")
+
+        except KeyError:
+            raise RuntimeError("API key missing in secrets.toml")
